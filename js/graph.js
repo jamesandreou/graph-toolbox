dbd195dedd42b191b3fd785b9b4fa67fe78f201b
jamesandreou <jandreou25@gmail.com>
2015-11-22 16:24:48 -0500

    Designed tool bar assets, created toolbar and implemented dynamic resizing

--- js/graph.js
+++ js/graph.js
@@ -44,7 +44,7 @@ function draw(app){
 
 function drawBackground(app){
 	var w = app['canvas'].width, h = app['canvas'].height;
-	var gridSize = Math.min(w, h) / 40;
+	var gridSize = Math.min(w, h) / 20;
 	app['2d'].strokeStyle = '#add8e6';
 	// Grid
 	for(var x = 0; x < w; x += gridSize){
@@ -64,7 +64,7 @@ function drawBackground(app){
 }
 
 function drawVertice(app, v){
-	var r = app['canvas'].width / 80;
+	var r = Math.min(app['canvas'].width, app['canvas'].height) / 40;
 	app['2d'].font = (r * 1.25) + 'px Georgia';
 	app['2d'].textBaseline = 'middle';
 	app['2d'].textAlign = 'center';
@@ -93,17 +93,9 @@ function drawEdge(app, e){
 /* Event Listeners */
 
 function initEventListeners(app){
-	$('#sel-btn').click(function (){
-		app['tools'] = 1;
-	});
-	$('#addv-btn').click(function (){
-		app['tools'] = 2;
-	});
-	$('#adde-btn').click(function (){
-		app['tools'] = 3;
-	});
-	$('#del-btn').click(function (){
-		app['tools'] = 4;
+	$(window).resize(function() { resize(app);});
+	$('.tool').click(function(){
+		selectTool(app, this);
 	});
 	$("#canvas").mousedown(function(e){
 		e.preventDefault();
@@ -131,6 +123,33 @@ function initEventListeners(app){
  	});
 }
 
+function selectTool(app, tool){
+	var id = $(tool).attr('id');
+	if(id === 'sel-btn'){
+		app['tool'] = 1;
+	}else if(id === 'addv-btn'){
+		app['tool'] = 2;
+	}else if(id === 'adde-btn'){
+		app['tool'] = 3;
+	}else if(id === 'edgew-btn'){
+		app['tool'] = 4;
+	}else if(id === 'edged-btn'){
+		app['tool'] = 5;
+	}else if(id === 'del-btn'){
+		app['tool'] = 6;
+	}
+	$('.tool > img').each(function (){
+		var oldSrc = $(this).attr('src');
+		var newSrc = oldSrc.substring(0, oldSrc.length - 5);
+		if($(this).parent().attr('id') === id){
+			newSrc += '0.svg';
+		}else{
+			newSrc += '1.svg';
+		}
+		$(this).attr('src', newSrc);
+	});
+}
+
 function moveObject(app, x, y){
 	if(app['boundType'] === 'v'){
  		app['bound'].x = x;
@@ -141,15 +160,31 @@ function moveObject(app, x, y){
 
 /* Helper functions for UI */
 
-function resizeCanvas(app){
+function resize(app){
 	document.documentElement.style.overflow = 'hidden';
     document.body.scroll = "no"; 
-	app['canvas'].width = $('.canvas-container').width();
-	app['canvas'].height = $('.canvas-container').height();
+    var w = $('.canvas-container').width(); 
+    var h = $('.canvas-container').height();
+	app['canvas'].width = w;
+	app['canvas'].height = h;
+	$('.tool > img').width($('.panel').width() * 0.25);
+	$('.tool').width(($('.panel').width() - 24) / 3);
+	var r = Math.min(app['canvas'].width, app['canvas'].height) / 40;
+	// If vertices are outside the canvas after resize shift back in
+	for(var i = 0; i < app['g'].v.length; i++){
+		var v  = app['g'].v[i];
+		if(v.x > w - r - 2){
+			v.x -= (v.x - w) + r + 10;
+		}else if(v.y > h - r - 2){
+			v.y -= (v.y - h) + r + 10;
+		}
+	}
+	draw(app);
+	draw(app);
 }
 
 function selectObject(app, x, y){
-	var r = app['canvas'].width / 80;
+	var r = Math.min(app['canvas'].width, app['canvas'].height) / 40;
 	for(var i = 0; i < app['g'].v.length; i++){
 		var v = app['g'].v[i];
 		app['2d'].beginPath();
@@ -183,14 +218,15 @@ $(window).ready(function(){
 	var app = {'canvas' : document.getElementById('canvas'), 
 				'2d' : canvas.getContext('2d'),
 				'g' : {v : [], e : []},
-				'tool' : 1, // 1 - Select, 2 - Add Vertice, 3 - Add Edge, 4 - Delete
+				'tool' : 1, // 1 - Select, 2 - Add Vertice, 3 - Add Edge, 4 - Edge Weight, 5 - Edge Dir, 6 - Delete
 				'bound' : null,
 				'boundType' : null
 			};
-	resizeCanvas(app);
+	resize(app);
 	initEventListeners(app);
 	app['g'].v.push(new Vertice("1", 100, 100, '#32cd32'));
 	app['g'].v.push(new Vertice("2", 400, 200, '#ff0000'));
 	app['g'].e.push(new Edge(app['g'].v[0], app['g'].v[1], null, null));
+	draw(app);
 	draw(app);
 });
